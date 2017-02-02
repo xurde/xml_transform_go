df3a50884f8d2094c70d22ea580617dc9fa684da
Matias Insaurralde <matias@insaurral.de>
2017-02-03 00:49:06 +0200

    Add init stuff.

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jbowtie/gokogiri/xml"
 	"github.com/jbowtie/ratago/xslt"
 )
 
+var globalStyle *xml.XmlDocument
+var globalStylesheet *xslt.Stylesheet
+
+func init() {
+	fmt.Println("init")
+	globalStyle, _ = xml.ReadFile("test.xsl", xml.StrictParseOption)
+	globalStylesheet, _ = xslt.ParseStylesheet(globalStyle, "test.xsl")
+}
+
 func doTransformation() {
-
 	style, _ := xml.ReadFile("test.xsl", xml.StrictParseOption)
-
 	doc, _ := xml.ReadFile("as.xml", xml.StrictParseOption)
-
 	stylesheet, _ := xslt.ParseStylesheet(style, "test.xsl")
-	// fmt.Println(stylesheet, err)
-
 	stylesheet.Process(doc, xslt.StylesheetOptions{true, nil})
-
 	// fmt.Println(output)
+}
+
+func doCachedTransformation() {
+	doc, _ := xml.ReadFile("as.xml", xml.StrictParseOption)
+	globalStylesheet.Process(doc, xslt.StylesheetOptions{true, nil})
 }
